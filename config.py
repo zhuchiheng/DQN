4178ca64ec2838eb8516931ed7a9eef9f2bb2855
zhuchiheng <chiheng.zhu@qq.com>
2017-03-16 16:55:49 +0800

    Update config.py
    fix paths problems

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from keras.layers import Input, Lambda
 class DDPGConfig(object):
     def __init__(self, n_stock):
         self.device = '/gpu:0'
-        self.save_path = '/home/tomoaki/work/github/jjakimoto.github.io/content'
+        self.save_path = './model/content'
         self.is_load = False
         self.gamma = 1.0
         self.history_length = 10
@@ -47,7 +47,7 @@ class DDPGConfig(object):
 class DQNConfig(object):
     def __init__(self, n_stock):
         self.device = '/gpu:0'
-        self.save_path = '/path/to/your/save/path/model.ckpt'
+        self.save_path = './model/model.ckpt'
         self.is_load = False
         self.gamma = 0.999
         self.history_length = 10
